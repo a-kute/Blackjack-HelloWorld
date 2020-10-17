9d3f7035b989e1b38ffd8c8fb98d87093681a15b
Josh Stevenson <steve291@purdue.edu>
2020-10-17 13:32:23 -0400

    Added the card class

--- src/main/Card.java
+++ src/main/Card.java
@@ -1,4 +1,27 @@
 package main;
 
 public class Card {
+    private String denomination;
+    private String suit;
+
+    public Card(String denomination, String suit) {
+        this.denomination = denomination;
+        this.suit = suit;
+    }
+
+    public String getDenomination() {
+        return this.denomination;
+    }
+
+    public String getSuit() {
+        return this.suit;
+    }
+
+    public void setDenomination(String denomination) {
+        this.denomination = denomination;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
 }
