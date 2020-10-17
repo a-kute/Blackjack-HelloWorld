f57091e7c837a73eee0c50af86837c347481738d
Henrik Berg <2002henrik.berg@gmail.com>
2020-10-17 14:42:45 -0400

    Start homeframe

--- src/gui/HomeFrame.java
+++ src/gui/HomeFrame.java
@@ -1,4 +1,24 @@
 package gui;
 
-public class HomeFrame {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HomeFrame extends JFrame implements ActionListener {
+
+
+    public HomeFrame() {
+        super("Blackjack | Home");
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public static void main(String[] args) {
+        HomeFrame homeFrame = new HomeFrame();
+    }
 }
