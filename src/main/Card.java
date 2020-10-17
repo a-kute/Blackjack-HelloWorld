3121ffc875b9c718a4a813dae4c99a516df4892b
Josh Stevenson <steve291@purdue.edu>
2020-10-17 15:40:15 -0400

    added the getImage class to Card.java

--- src/main/Card.java
+++ src/main/Card.java
@@ -1,5 +1,10 @@
 package main;
 
+import java.awt.Image;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import java.io.*;
+
 public class Card {
     private int denomination;
     private String suit;
@@ -23,5 +28,73 @@ public class Card {
 
     public void setSuit(String suit) {
         this.suit = suit;
+    }
+
+    public ImageIcon getImage() throws IOException {
+        String singleCharDenomination = null;
+        switch (denomination) {
+            case 1:
+                singleCharDenomination = "1";
+                break;
+            case 2:
+                singleCharDenomination = "2";
+                break;
+            case 3:
+                singleCharDenomination = "3";
+                break;
+            case 4:
+                singleCharDenomination = "4";
+                break;
+            case 5:
+                singleCharDenomination = "5";
+                break;
+            case 6:
+                singleCharDenomination = "6";
+                break;
+            case 7:
+                singleCharDenomination = "7";
+                break;
+            case 8:
+                singleCharDenomination = "8";
+                break;
+            case 9:
+                singleCharDenomination = "9";
+                break;
+            case 10:
+                singleCharDenomination = "10";
+                break;
+            case 11:
+                singleCharDenomination = "J";
+                break;
+            case 12:
+                singleCharDenomination = "Q";
+                break;
+            case 13:
+                singleCharDenomination = "K";
+                break;
+            case 14:
+                singleCharDenomination = "A";
+                break;
+        }
+
+        String singleCharSuit = null;
+        switch(suit) {
+            case "clubs":
+                singleCharSuit = "C";
+                break;
+            case "hearts":
+                singleCharSuit = "H";
+                break;
+            case "diamonds":
+                singleCharSuit = "D";
+                break;
+            case "spades":
+                singleCharSuit = "S";
+                break;
+        }
+
+        File sourceimage = new File("Blackjack-HelloWorld/images/" + singleCharDenomination + singleCharSuit + ".png");
+        Image image = ImageIO.read(sourceimage);
+        return new ImageIcon(image);
     }
 }
